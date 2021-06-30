653231dc60e0ac826d3cc79bc18ab9dbef4bb8ea
saltsuica <crkylin@gmail.com>
2021-06-30 14:19:41 +0800

    add mathtools

--- Assets/Shaders/Tools/MathTools.hlsl
+++ Assets/Shaders/Tools/MathTools.hlsl
@@ -4,3 +4,8 @@ float cubicPulse( float c, float w, float x ){
     x /= w;
     return 1.0 - x*x*(3.0-2.0*x);
 }
+
+float concaveFuncA(float x)
+{
+    return 0.8 * smoothstep(0.3,-0.1,x+0.6) + smoothstep(1,2,x+0.6);
+}
